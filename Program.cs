dfb5f9b3f519e6dbbea4bb20d32935c2a807a1f2
ROSTAND LELE <rostoinfocus@gmail.com>
2024-12-02 22:30:36 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace App
+{
+    class NomDeClasse
+    {
+        // Champs
+        private int champ;
+
+        // Propriétés
+        public int Propriete { get; set; }
+
+        // Méthodes
+        public void Methode()
+        {
+            // Code
+        }
+    }
+
+
+}
