126d3dca3885ae3aafd64c3a2007a24badba1dd3
ROSTAND LELE <rostoinfocus@gmail.com>
2024-12-13 11:02:16 +0100

    dee
    deer

--- Program.cs
+++ Program.cs
@@ -1,21 +1,33 @@
 ﻿using System;
 
-namespace App
+public class Boite<T>
 {
-    class NomDeClasse
+    private T contenu; // Champ privé pour stocker le contenu de type générique T
+
+    // Méthode pour ajouter un élément de type T à la boite
+    public void Ajouter(T item)
     {
-        // Champs
-        private int champ;
-
-        // Propriétés
-        public int Propriete { get; set; }
-
-        // Méthodes
-        public void Methode()
-        {
-            // Code
-        }
+        contenu = item;
     }
 
+    // Méthode pour obtenir l'élément de type T de la boite
+    public T Obtenir()
+    {
+        return contenu;
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Utilisation de la classe générique Boite avec un entier
+        Boite<int> boiteInt = new Boite<int>();
+        boiteInt.Ajouter(5);
+        Console.WriteLine(boiteInt.Obtenir()); // Affiche 5
 
+        // Utilisation de la classe générique Boite avec une chaîne de caractères
+        Boite<string> boiteString = new Boite<string>();
+        boiteString.Ajouter("Bonjour");
+        Console.WriteLine(boiteString.Obtenir()); // Affiche "Bonjour"
+    }
 }
--- obj/Release/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ obj/Release/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dfb5f9b3f519e6dbbea4bb20d32935c2a807a1f2")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
